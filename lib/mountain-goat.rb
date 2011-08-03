97ff156713b8931264d2428fcbaacc30857e742e
Geoff Hayes <hayesgm@gmail.com>
2011-08-03 06:43:26 -0500

    verify class variable exists before using

--- lib/mountain-goat.rb
+++ lib/mountain-goat.rb
@@ -26,11 +26,13 @@ require File.join([File.dirname(__FILE__), 'mountain-goat/models/rally'])
 
 class MountainGoat
   def self.add_meta_option(option, &block)
+    @@meta_options = {} if !defined?(@@meta_options)
     @@meta_options ||= {}
     @@meta_options.merge!({ option => block })
   end
   
   def self.get_meta_options
-    @@meta_options
+    return {} if !defined?(@@meta_options)
+    @@meta_options || {}
   end 
 end
