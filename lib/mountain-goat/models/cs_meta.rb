88a63d572c104963a94920d30ca7624486212dc2
Geoff Hayes <hayesgm@gmail.com>
2011-09-06 16:28:09 -0500

    Introduce multi-armed bandit testing && Moutain Goat Reporting

--- lib/mountain-goat/models/cs_meta.rb
+++
@@ -1,9 +0,0 @@
-class CsMeta < ActiveRecord::Base
-  
-  belongs_to :convert_meta_type
-  belongs_to :rally
-  
-  validates_presence_of :convert_meta_type_id
-  validates_presence_of :rally_id
-  
-end
