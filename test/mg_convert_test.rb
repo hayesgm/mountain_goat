ea72f29ac0a59aae6ff8e1de289a535df2d42a8a
Geoff Hayes <hayesgm@gmail.com>
2011-09-07 00:03:39 -0500

    updates for meta tracking bug

---
+++ test/mg_convert_test.rb
@@ -0,0 +1,24 @@
+require File.join(File.dirname(__FILE__), 'test_helper')
+
+class Mg::ConvertTest < ActiveSupport::TestCase
+  include MetricTracking::Controller
+  
+  test "should tests" do
+    @mg_strategy = 'e-greedy'
+    
+    assert_difference 'Mg::Convert.count' do
+      assert_difference 'Mg::Rally.count' do
+        assert_difference 'Mg::CsMeta.count' do
+          assert_difference 'Mg::ConvertMetaType.count' do
+            rw(:hiz, 0, :geoff => "hayes")
+          end
+        end
+      end
+    end
+      
+    conv = Mg::Convert.by_type(:hiz)
+    assert_not_nil conv
+    
+    assert_equal "hayes", conv.rallies_for_meta(:geoff).first.first[0]
+  end
+end
